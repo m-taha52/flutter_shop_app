ea1f87d19417b573c1538ea06fb48dfba365937e
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-10-24 15:35:16 +0300

    size constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/app.dart';
 import 'package:flutter/material.dart';
+import "package:ecommerce_app/utils/constants/colors.dart"
 
 void main() {
   runApp(const App());
