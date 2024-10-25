4c3c41cfadea7c9197e66a5a0aa185a8f3aff50a
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-10-25 11:02:02 +0300

    removed an import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/app.dart';
 import 'package:flutter/material.dart';
-import "package:ecommerce_app/utils/constants/colors.dart"
 
 void main() {
   runApp(const App());
