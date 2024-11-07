ba37e3f069b6f975626fa024c6220d2e1df25c98
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-11-07 12:34:34 +0300

    Backend for Splash Screen

---
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  //variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  void updatePageIndicator(index) {
+    currentPageIndex.value = index;
+  }
+
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300), // Adjust duration as needed
+      curve: Curves.easeInOut, // Add a smooth transition
+    );
+  }
+
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      //Get.to(LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+}
