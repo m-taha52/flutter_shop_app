6aeebd07f6ec2672dc96eeb88ad0a52ff9dbd49e
Mohammed Taha Alibhai <64618100+m-taha52@users.noreply.github.com>
2024-12-16 11:11:38 +0000

    Update home.dart
    Added a title to the Grid Layout section. Added constants to improve performance

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
               child: Column(
               children: [
                         //App Bar
@@ -49,11 +49,17 @@ class HomeScreen extends StatelessWidget {
 
             //Body 
             Padding(
-              padding: EdgeInsets.all(TSizes.defaultSpace),
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
               child: Column(
                 children: [
-                  TPromoSlider(banners: [TImages.promoBanner1, TImages.promoBanner2, TImages.promoBanner3],),
-                  SizedBox(height: TSizes.spaceBtwItems,),
+
+                  //Promo Slider
+                  const TPromoSlider(banners: [TImages.promoBanner1, TImages.promoBanner2, TImages.promoBanner3],),
+                  const SizedBox(height: TSizes.spaceBtwItems,),
+
+                  //Heading
+                  TSectionHeading(title: 'Popular Products', onPressed: () {},),
+                  const SizedBox(height: TSizes.spaceBtwItems,),
 
                   TGridLayout(itemCount: 2, itemBuilder: (_, index) => const TProductCardVertical())
                 ],
