5f98da9802a9e182d17a3396b8e74147e8403cdd
Mohammed Taha Alibhai <64618100+m-taha52@users.noreply.github.com>
2025-01-12 08:47:14 +0000

    Create t_circular_image.dart

---
+++ lib/common/widgets/image/t_circular_image.dart
@@ -0,0 +1,54 @@
+import "package:ecommerce_app/utils/constants/colors.dart";
+import "package:ecommerce_app/utils/constants/image_strings.dart";
+import "package:ecommerce_app/utils/constants/sizes.dart";
+import "package:ecommerce_app/utils/helpers/helper_functions.dart";
+import "package:flutter/material.dart";
+
+
+class TCircularImage extends StatelessWidget {
+  const TCircularImage({
+    super.key, 
+    this.boxFit = BoxFit.cover, 
+    required this.image, 
+    this.isNetworkImage = false, 
+    this.overlayColor, 
+    this.backgroundColor, 
+    this.width = 56, 
+    this.height = 56, 
+    this.padding = TSizes.sm,
+  });
+
+  final BoxFit? boxFit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double width, height, padding;
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      width: width,
+      height: height,
+      padding:  EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: dark ? TColors.black : TColors.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+     
+      child: Center(
+        child: Image(
+          fit: boxFit,
+          image: isNetworkImage ? NetworkImage(image) : AssetImage(image) as ImageProvider,
+           color: overlayColor,
+          ),
+      ),
+    
+    
+    );
+  }
+}
