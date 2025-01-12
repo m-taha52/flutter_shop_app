b49b7dff6d79f36416a417e114088b3ed4f9bfac
Mohammed Taha Alibhai <64618100+m-taha52@users.noreply.github.com>
2025-01-12 08:46:53 +0000

    Update search_container.dart
    made the search container more reusable

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -8,13 +8,20 @@ import "package:iconsax/iconsax.dart";
 
 class TSearchContainer extends StatelessWidget {
   const TSearchContainer({
-    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true,  this.showBorder = true, this.onTap
+    super.key, 
+    required this.text, 
+    this.icon = Iconsax.search_normal, 
+    this.showBackground = true,  
+    this.showBorder = true,
+     this.onTap,
+     this.padding = const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace)
   });
   
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
   final VoidCallback? onTap;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +30,7 @@ class TSearchContainer extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        padding: padding,
         child: Container(
           width: TDeviceUtils.getScreenWidth(context),
           padding: const EdgeInsets.all(TSizes.md),
