942f08cc32b866c7a5230b7a17ef707b003c2ee2
Mohammed Taha Alibhai <64618100+m-taha52@users.noreply.github.com>
2025-01-14 18:34:13 +0000

    Update section_headinf.dart
    Fixed a bug where a static value was used instead of a variable

--- lib/common/widgets/text/section_heading.dart
+++ lib/common/widgets/text/section_heading.dart
@@ -16,7 +16,7 @@ class TSectionHeading extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text("Popular Categories", style: Theme.of(context).textTheme.headlineSmall!.apply(color: textColor), maxLines: 1, overflow: TextOverflow.ellipsis,),
+        Text(title, style: Theme.of(context).textTheme.headlineSmall!.apply(color: textColor), maxLines: 1, overflow: TextOverflow.ellipsis,),
         if(showActionButton) TextButton(onPressed: onPressed, child:  Text(buttonTitle)),
       ],
     );
