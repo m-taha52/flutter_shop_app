626543f468b7216cd1eadba33e5391483c70acb8
Mohammed Taha Alibhai <64618100+m-taha52@users.noreply.github.com>
2025-01-14 18:36:17 +0000

    Create brand_card.dart
    Created a new component that showcases the  brand title along with a verified icon

---
+++ lib/common/widgets/brands/brand_card.dart
@@ -0,0 +1,62 @@
+import "package:ecommerce_app/common/widgets/custom_shapes/containers/rounded_container.dart";
+import "package:ecommerce_app/common/widgets/image/t_circular_image.dart";
+import "package:ecommerce_app/common/widgets/text/t_brand_title_text_with_verified_icon.dart";
+import "package:ecommerce_app/utils/constants/colors.dart";
+import "package:ecommerce_app/utils/constants/enums.dart";
+import "package:ecommerce_app/utils/constants/image_strings.dart";
+import "package:ecommerce_app/utils/constants/sizes.dart";
+import "package:ecommerce_app/utils/helpers/helper_functions.dart";
+import "package:flutter/material.dart";
+
+
+class TBrandCard extends StatelessWidget {
+  const TBrandCard({
+    super.key, 
+    required this.showBorder, 
+    this.onTap,
+  });
+
+  final bool showBorder;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return GestureDetector(
+    onTap: onTap,
+    child: TRoundedContainer(
+      padding: const EdgeInsets.all(TSizes.sm),
+      showBorder: showBorder,
+      backgroundColor: Colors.transparent,
+      child: Row(children: [
+        ///Icon
+        Flexible(
+          child: TCircularImage(
+            isNetworkImage: false,
+            image: TImages.clothIcon,
+            backgroundColor: Colors.transparent,
+            overlayColor: dark ? TColors.white : TColors.dark,
+          ),
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems / 2,),
+    
+        ///Text
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TBrandTitleWithVerifiedIcon(title: 'Nike', brandTextSize: TextSizes.large,),
+              Text(
+                '256 products',
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.labelMedium,
+              )
+            ],
+          ),
+        )
+      ],)
+    ),
+                          );
+  }
+}
